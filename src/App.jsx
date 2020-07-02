e9d27ca30f37b56e0879c06d7d3eb70bb513dd2a
Matthew Milici <milici.m915@gmail.com>
2020-07-02 08:06:08 -0700

    Added navar

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Header from "./component/header/header";
 import AboutMe from "./component/about-me/about-me";
 import Resume from "./component/work-experience/work-experience";
 import ContactMe from "./component/contact-me/contact-me";
+import Navbar from "./component/navbar/navbar";
 
 function App() {
 	return (
 		<>
+			<Navbar />
 			<Header />
 			<AboutMe />
 			<Resume />
---
+++ src/component/navbar/navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Navbar() {
+	return (
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button
+						type="button"
+						class="navbar-toggle"
+						data-toggle="collapse"
+						data-target="#myNavbar"
+					>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">
+						Milici
+					</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav">
+						<li>
+							<a href="#">About Me</a>
+						</li>
+						<li>
+							<a href="#">Experience</a>
+						</li>
+						<li>
+							<a href="#">Resume</a>
+						</li>
+						<li>
+							<a href="#">Contact Me</a>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="#">
+								<span class="glyphicon glyphicon-user"></span> Github
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
